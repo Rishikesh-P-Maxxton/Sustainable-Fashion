144e836d661f513116eff5661d47840234cafd38
Rishikesh Puri <r.puri@maxxton.onmicrosoft.com>
2024-07-31 16:17:42 +0530

    nested Routing works, and contact us submenu added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactElement1Component } from './contact-element1/contact-element1.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'contact', component: ContactPageComponent,
+    children:[
+      { path: '', redirectTo: 'contactus', pathMatch: 'full', outlet: 'contactPage' },
+      {path: 'contactus', component:ContactElement1Component, outlet:'contactPage'},
+      {path: 'faq', component:FaqComponent, outlet:'contactPage'}
+
+    ]
+   },
+ 
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export { RouterModule };
---
+++ src/app/contact-element1/contact-element1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-element1',
+  templateUrl: './contact-element1.component.html',
+  styleUrl: './contact-element1.component.css'
+})
+export class ContactElement1Component {
+
+}
