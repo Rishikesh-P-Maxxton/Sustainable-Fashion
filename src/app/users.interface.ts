f979fa773cf700a450d6b10696c85e9b9df0f0d4
Rishikesh Puri <r.puri@maxxton.onmicrosoft.com>
2024-08-02 17:08:10 +0530

    Auto-generated commit

--- src/app/users.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Users {
-    name: string;
-    email: string ;// required with minimum 5 characters
-    address: {
-    street: string; // required
-    postcode: string;
-    }
-}
